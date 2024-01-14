0d56d9c0ff987873641192930b35e432e4a7d2e7
Andres Lujan <andreslujandev@gmail.com>
2024-01-13 19:25:14 -0500

    Init repo

--- sharingan/src/routes/index.ts
+++ sharingan/src/routes/index.ts
@@ -4,7 +4,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello, World!');
 });
 
 export default router;
