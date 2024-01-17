a45bcc25e3eb62131b3058d48299e5d9e8f14f89
Andres Lujan <andreslujandev@gmail.com>
2024-01-16 19:44:12 -0500

    WIP

--- sharingan/src/api/models/job.ts
+++ sharingan/src/api/models/job.ts
@@ -57,17 +57,3 @@ Job.init({
 });
 
 export default Job;
-
-
-export function serializeJobOutput(job: JobOutput): any {
-  return {
-    id: job.id,
-    title: job.title,
-    description: job.description,
-    data: job.data,
-    runAt: job.runAt.toISOString(),
-    createdAt: job.createdAt?.toISOString(),
-    updatedAt: job.updatedAt?.toISOString(),
-    deletedAt: job.deletedAt?.toISOString()
-  };
-}
